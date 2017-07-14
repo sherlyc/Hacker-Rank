8de1af75a7528d3b0602c29cf78de2897c1b2a5d
Sherly Chan <sherlyc@gmail.com>
2017-07-14 22:10:46 +1200

    Create birthday-cake-candles.js

---
+++ algorithms/warmup/birthday-cake-candles.js
@@ -0,0 +1,35 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function birthdayCakeCandles(n, ar) {
+    // Complete this function
+    let max = Math.max(...ar)
+    return ar.filter(function(el){return el==max }).length
+}
+
+function main() {
+    var n = parseInt(readLine());
+    ar = readLine().split(' ');
+    ar = ar.map(Number);
+    var result = birthdayCakeCandles(n, ar);
+    process.stdout.write("" + result + "\n");    
+}
